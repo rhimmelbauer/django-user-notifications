08707e26bf887ab5d40cb3a84479590c15cae635
Roberto Himmelbauer <robertoh89@gmail.com>
2021-11-15 17:05:26 -0800

    removed signals and rule_processor files, this should be implemented in the base project not on the package itself

--- user_notifications/signals.py
+++
@@ -1,9 +0,0 @@
-from allauth.account.signals import user_logged_in
-from django.dispatch import receiver
-
-from .utils import get_site_from_request
-
-
-@receiver(user_logged_in)
-def process_rules(sender, request, user, **kwargs):
-    # Insert any logic here to add desired notifications for users upon login.
