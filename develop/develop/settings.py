ff825b4d73ba04f43280d28e514abdc6b68e4538
Roberto Himmelbauer <robertoh89@gmail.com>
2022-01-31 12:30:13 -0800

    deleted migration to change primary key field form BigAuto to Auto

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -153,5 +153,5 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 LOGIN_REDIRECT_URL = "/"
